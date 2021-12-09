0020c5b4788b42ec9ffb66810ce7b257b67da078
Sonkeng Maldini <sdmg15@pm.me>
2021-12-09 15:59:20 +0100

    feat: more tests for the addAuthenticator

--- main.ts
+++ main.ts
@@ -26,10 +26,9 @@ const getPermission = async (request: any) => {
  * @param prvkey 
  * @returns
  */
-export async function initialize(prvKey: String): Promise<any> {
-    const authId = "gd-3id-ceramic"+ randomString(5);  
+export async function initialize(prvKey: string): Promise<any> {
+    const authId = "gd-3id-ceramic-"+ randomString(5);  
     const authSecret = Buffer.from(prvKey);
-
     const threeIdProvider = await ThreeIdProvider.create({
         getPermission,  
         ceramic,
@@ -42,29 +41,27 @@ export async function initialize(prvKey: String): Promise<any> {
     const did = new DID({ provider, resolver });
     ceramic.did = did;
     const res = await ceramic.did.authenticate();
-
     // Now encrypt to prvkey with the master seed and store it inside a field `masterSeed`
-
-    return res;
+    return {authId, did: res};
 } 
 
 /**
  * Add the given private key as another authSecret that can access the DID seed
  * @param prvKey an authSecret already present inside the keychain
  * @param newPrvKey the new key to be added inside the keychain
- * @returns bool indicating whether the addition was successful or not
  */
-export async function addAuthenticator(prvKey: String, newPrvKey: String): Promise<void> {
-    const authId = "gd-3id-ceramic"+ randomString(5);  
+export async function addAuthenticator(prevAuthId: string, prvKey: string, newPrvKey: string): Promise<void> {
+    const authId = "gd-3id-ceramic-"+ randomString(5);  
     const authSecret = Buffer.from(prvKey);
     const newAuthSecret = Buffer.from(newPrvKey);
     const threeIdProvider = await ThreeIdProvider.create({
         getPermission,  
         ceramic,
-        authId,
         authSecret,
+        authId: prevAuthId,
     });
-    await threeIdProvider.keychain.add('auth2', newAuthSecret);
+    
+    await threeIdProvider.keychain.add(authId, newAuthSecret);
     await threeIdProvider.keychain.commit();
 }
 
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { randomString } from '@stablelib/random'
 import { addAuthenticator, initialize } from '../main';
 
 describe("Hello", () => {
@@ -15,22 +16,32 @@ describe("Hello", () => {
 
 describe("Initialize a new Decentralized Identifier.", () => {
     it("Should initialize a DID based on privatekey", async (done) => {
-        var myPrvkey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // Length need to be 32
-        var res = await initialize(myPrvkey);
-        expect(res).to.equal("did:3:kjzl6cwe1jw1494fmr39v7jqm87qkukqt38cf6fvtk091qcl795u1n4okkj5b7w");
+        const myPrvkey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // Length need to be 32
+        const res = await initialize(myPrvkey);
+        expect(res.did).to.equal("did:3:kjzl6cwe1jw1494fmr39v7jqm87qkukqt38cf6fvtk091qcl795u1n4okkj5b7w");
     }).timeout(50000);
 });
 
 describe.only("Add a new private key as authSecret", () => {
     it("Should retrieve the same DID for two different authSecrents", async (done) => {
-        var myPrvkey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // Length need to be 32
-        var myNewPrvkey = "1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1"; // Length need to be 32
-        var res = await initialize(myPrvkey);
-        expect(res).to.equal("did:3:kjzl6cwe1jw1494fmr39v7jqm87qkukqt38cf6fvtk091qcl795u1n4okkj5b7w");
-        await addAuthenticator(myPrvkey, myNewPrvkey);
+        const myPrvkey = "7yaAGVXHhcLFGXL3uQV0Ne85CDFlZjJa"; // Length need to be 32
+        const myNewPrvkey = "RIqUaX127GgnirqYVfMKospZRS3wBhgJ"; //  Length need to be 32
+        
+        const res = await initialize(myPrvkey);
+        await addAuthenticator(res.authId, myPrvkey, myNewPrvkey);
 
-        var resNewPrvKey = await initialize(myNewPrvkey);
-        expect(resNewPrvKey).to.equal(res);
+        const resNewPrvKey = await initialize(myNewPrvkey);
+        expect(resNewPrvKey.did).to.equal(res.did); // gd-3id-ceramic-oWaCf and gd-3id-ceramic-LuAYd
+    }).timeout(80000);
 
-    }).timeout(50000);
+    it.only("Should fail since the key was not added as authSecret", async() => {
+        const myPrvkey = "7yaAGVXHhcLFGXL3uQV0Ne85CDFlZjJa"; // Length need to be 32
+        const myNewPrvkey = "PIqUaX127GgnirqYVfMKospZRS3wBhgJ"; //  Length need to be 32
+        const res = await initialize(myPrvkey);
+
+        // myNewPrvkey was not added inside the list of authenticator so this will fail
+        const resNewPrvKey = await initialize(myNewPrvkey);
+        expect(resNewPrvKey.did).not.to.equal(res.did);
+    }).timeout(90000);
+
 });
