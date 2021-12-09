9767e172e0fd802526ce9652ca011b0a75018385
Sonkeng Maldini <sdmg15@pm.me>
2021-12-09 12:03:06 +0100

    feat: addAuthenticator function

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { randomBytes } from '@stablelib/random'
+import { randomString } from '@stablelib/random'
 import { CeramicClient } from '@ceramicnetwork/http-client'
 import KeyDidResolver from 'key-did-resolver'
 import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
@@ -27,7 +27,7 @@ const getPermission = async (request: any) => {
  * @returns
  */
 export async function initialize(prvKey: String): Promise<any> {
-    const authId = "gd-3id-ceramic";
+    const authId = "gd-3id-ceramic"+ randomString(5);  
     const authSecret = Buffer.from(prvKey);
 
     const threeIdProvider = await ThreeIdProvider.create({
@@ -42,15 +42,30 @@ export async function initialize(prvKey: String): Promise<any> {
     const did = new DID({ provider, resolver });
     ceramic.did = did;
     const res = await ceramic.did.authenticate();
+
+    // Now encrypt to prvkey with the master seed and store it inside a field `masterSeed`
+
     return res;
 } 
 
 /**
  * Add the given private key as another authSecret that can access the DID seed
- * @param prvKey 
+ * @param prvKey an authSecret already present inside the keychain
+ * @param newPrvKey the new key to be added inside the keychain
+ * @returns bool indicating whether the addition was successful or not
  */
-export function addAuthenticator(prvKey: String): any {
-
+export async function addAuthenticator(prvKey: String, newPrvKey: String): Promise<void> {
+    const authId = "gd-3id-ceramic"+ randomString(5);  
+    const authSecret = Buffer.from(prvKey);
+    const newAuthSecret = Buffer.from(newPrvKey);
+    const threeIdProvider = await ThreeIdProvider.create({
+        getPermission,  
+        ceramic,
+        authId,
+        authSecret,
+    });
+    await threeIdProvider.keychain.add('auth2', newAuthSecret);
+    await threeIdProvider.keychain.commit();
 }
 
 /**
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { initialize } from '../main';
+import { addAuthenticator, initialize } from '../main';
 
 describe("Hello", () => {
     describe("World", () => {
@@ -18,5 +18,19 @@ describe("Initialize a new Decentralized Identifier.", () => {
         var myPrvkey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // Length need to be 32
         var res = await initialize(myPrvkey);
         expect(res).to.equal("did:3:kjzl6cwe1jw1494fmr39v7jqm87qkukqt38cf6fvtk091qcl795u1n4okkj5b7w");
+    }).timeout(50000);
+});
+
+describe.only("Add a new private key as authSecret", () => {
+    it("Should retrieve the same DID for two different authSecrents", async (done) => {
+        var myPrvkey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // Length need to be 32
+        var myNewPrvkey = "1aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1"; // Length need to be 32
+        var res = await initialize(myPrvkey);
+        expect(res).to.equal("did:3:kjzl6cwe1jw1494fmr39v7jqm87qkukqt38cf6fvtk091qcl795u1n4okkj5b7w");
+        await addAuthenticator(myPrvkey, myNewPrvkey);
+
+        var resNewPrvKey = await initialize(myNewPrvkey);
+        expect(resNewPrvKey).to.equal(res);
+
     }).timeout(50000);
 });
