a5a3341cc6e8ede58659edb37f585d97fbcef4d9
sirpy <hadarr@gmail.com>
2022-01-04 17:59:12 +0200

    add: save label of authenticator

--- main.ts
+++ main.ts
@@ -1,139 +1,148 @@
 import { CeramicClient } from '@ceramicnetwork/http-client'
 import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
-import { TileDocument } from '@ceramicnetwork/stream-tile';
+import { TileDocument } from '@ceramicnetwork/stream-tile'
 
-import { encrypt, decrypt, getPublic } from "@toruslabs/eccrypto"
+import { encrypt, decrypt, getPublic } from '@toruslabs/eccrypto'
 
 import ThreeIdProvider from '3id-did-provider'
 import { DID } from 'dids'
-import { Ecies } from 'eccrypto';
+import { Ecies } from 'eccrypto'
 export class CeramicSDK {
-    threeIdProvider!: ThreeIdProvider
-    ceramic: CeramicClient;
-    authId!: string;
+  threeIdProvider!: ThreeIdProvider
+  ceramic: CeramicClient
+  authId!: string
 
-    constructor(networkUrl: string){
-        this.ceramic = new CeramicClient(networkUrl);
+  constructor(networkUrl: string) {
+    this.ceramic = new CeramicClient(networkUrl)
+  }
+
+  async getPermission(request: any) {
+    return request.payload.paths
+  }
+
+  async getMeta() {
+    const doc = await TileDocument.deterministic(this.ceramic, {
+      family: 'masterSeed',
+      tags: ['v2'],
+      controllers: [this.ceramic.did?.id as string]
+    })
+
+    return doc
+  }
+  /**
+   * Creates or retrieves a TileDocument containing the master seed field
+   *
+   * @param publickey
+   * @param prvkey the data to store inside masterSeed field
+   * @param did a DID that will control the stream
+   */
+  async initializeMasterSeed(prvkey: string, publicKey: string, label: string) {
+    const doc = await this.getMeta()
+
+    const docType = doc.content as Record<string, any>
+
+    if (docType.masterSeed) {
+      return doc
     }
 
-    async getPermission(request: any){
-        return request.payload.paths;
+    const seed = this.threeIdProvider.keychain._keyring.seed
+    const publicKeySeed = getPublic(Buffer.from(seed))
+    const encryptedData = await this.encrypt(publicKeySeed, prvkey)
+
+    await doc.update({ masterSeed: encryptedData, authenticators: { [publicKey]: label } })
+    return doc
+  }
+
+  async encrypt(publicKey: any, dataToEncrypt: any): Promise<any> {
+    const encrypted = await encrypt(Buffer.from(publicKey), Buffer.from(dataToEncrypt))
+    return encrypted
+  }
+
+  async decrypt(privateKey: Buffer, encryptedData: any): Promise<any> {
+    const decrypted = await decrypt(privateKey, encryptedData)
+    return decrypted
+  }
+
+  /**
+   * Initialize a DID based on private key and create a new one if none exists
+   * Will also encrypt the private key and store it inside masterSeed field
+   * @param prvKey the private key to use to create a new DID
+   * @param pubKey the public key derived from prvkey that will be used as authId
+   * @returns string
+   */
+  async initialize(prvKey: string, pubKey: string, label: string): Promise<any> {
+    const authSecret = Buffer.from(prvKey)
+    this.authId = pubKey
+    const config = {
+      getPermission: this.getPermission,
+      ceramic: this.ceramic,
+      authId: this.authId,
+      authSecret
+    }
+    this.threeIdProvider = await ThreeIdProvider.create(config)
+    const provider = this.threeIdProvider.getDidProvider()
+    const resolver = ThreeIdResolver.getResolver(this.ceramic)
+    this.ceramic.did = new DID({ provider, resolver })
+    const authenticatedDID = await this.ceramic.did.authenticate()
+
+    await this.initializeMasterSeed(prvKey, pubKey, label)
+    return authenticatedDID
+  }
+
+  /**
+   * Add the given private key as another authSecret that can access the DID seed
+   * @param newPrvKey the new key to be added inside the keychain
+   * @param pubKey the public key derived from prvkey that will be used as authId
+   */
+  async addAuthenticator(newPrvKey: string, pubKey: string, label: string): Promise<any> {
+    const newAuthSecret = Buffer.from(newPrvKey)
+    await this.threeIdProvider.keychain.add(pubKey, newAuthSecret)
+    await this.threeIdProvider.keychain.commit()
+    const doc = await this.getMeta()
+    const content = <Record<string, any>>doc.content
+
+    content.authenticators[pubKey] = label
+
+    await doc.update({ ...content })
+    return this.threeIdProvider
+  }
+
+  /**
+   * Return the decrypted master seed
+   * @returns string
+   */
+  async getMasterSeed(): Promise<string> {
+    const doc = await this.getMeta()
+    const tileContent = <Record<string, any>>doc.content
+
+    const ecies: Ecies = {
+      iv: Buffer.from(tileContent.masterSeed.iv.data),
+      ephemPublicKey: Buffer.from(tileContent.masterSeed.ephemPublicKey.data),
+      ciphertext: Buffer.from(tileContent.masterSeed.ciphertext.data),
+      mac: Buffer.from(tileContent.masterSeed.mac.data)
     }
 
-    /**
-     * Creates or retrieves a TileDocument containing the master seed field
-     * 
-     * @param publickey 
-     * @param prvkey the data to store inside masterSeed field
-     * @param did a DID that will control the stream
-     */
-    async initializeMasterSeed(prvkey: any) {
-        const doc = await TileDocument.deterministic(
-            this.ceramic,
-            { family: "masterSeed", 
-              tags: ["v1"],
-              controllers: [this.ceramic.did?.id as string]
-             }
-        );
+    const seed = Buffer.from(this.threeIdProvider.keychain._keyring.seed)
+    const decryptedMastedSeed = await this.decrypt(seed, ecies)
 
-        const docType =  doc.content as Record<string, any>;
+    return decryptedMastedSeed.toString()
+  }
 
-        if (docType.masterSeed) {
-            return doc;
-        }
+  /**
+   * Remove the provided authenticator
+   * @param pubKey the public key derived from prvkey that will be used as authId
+   * @returns bool
+   */
+  async removeAuthenticator(pubKey: string): Promise<any> {
+    await this.threeIdProvider.keychain.remove(pubKey)
+    await this.threeIdProvider.keychain.commit()
+    const doc = await this.getMeta()
+    const content = <Record<string, any>>doc.content
 
-        const seed = this.threeIdProvider.keychain._keyring.seed;
-        const publicKeySeed = getPublic(Buffer.from(seed));
-        const encryptedData = await this.encrypt(publicKeySeed, prvkey);
+    delete content.authenticators[pubKey]
 
-        await doc.update({masterSeed: encryptedData});      
-        return doc; 
-    }
+    await doc.update({ ...content })
 
-    async encrypt(publicKey: any, dataToEncrypt: any): Promise<any>{
-        const encrypted = await encrypt(Buffer.from(publicKey), Buffer.from(dataToEncrypt));
-        return encrypted;
-    }
-
-    async decrypt(privateKey: any, encryptedData: any): Promise<any> {
-        const decrypted = await decrypt(privateKey, encryptedData);
-        return decrypted;
-    }
-
-    /**
-     * Initialize a DID based on private key and create a new one if none exists
-     * Will also encrypt the private key and store it inside masterSeed field
-     * @param prvKey the private key to use to create a new DID
-     * @param pubKey the public key derived from prvkey that will be used as authId
-     * @returns string
-     */
-     async initialize(prvKey: string, pubKey: string): Promise<any> {
-        const authSecret = Buffer.from(prvKey);
-        this.authId = pubKey;
-        const config = {
-            getPermission: this.getPermission,  
-            ceramic: this.ceramic,
-            authId: this.authId,
-            authSecret
-        }
-        this.threeIdProvider = await ThreeIdProvider.create(config);
-        const provider = this.threeIdProvider.getDidProvider();
-        const resolver = ThreeIdResolver.getResolver(this.ceramic);
-        this.ceramic.did = new DID({ provider, resolver });
-        const authenticatedDID = await this.ceramic.did.authenticate();
-        
-        await this.initializeMasterSeed(prvKey);
-        return authenticatedDID;
-    } 
-
-    /**
-     * Add the given private key as another authSecret that can access the DID seed
-     * @param newPrvKey the new key to be added inside the keychain
-     * @param pubKey the public key derived from prvkey that will be used as authId
-     */
-    async addAuthenticator(newPrvKey: string, pubKey: string): Promise<any> {
-        const newAuthSecret = Buffer.from(newPrvKey);
-        await this.threeIdProvider.keychain.add(pubKey, newAuthSecret);
-        await this.threeIdProvider.keychain.commit();
-        return this.threeIdProvider;
-    }
-
-    /**
-     * Return the decrypted master seed
-     * @returns string
-     */
-    async getMasterSeed(): Promise<string> {
-        const doc = await TileDocument.deterministic(
-            this.ceramic,
-            { family: "masterSeed",
-              tags: ["v1"],
-              controllers: [this.ceramic.did?.id as string],
-             },
-             { anchor: false, publish: false }
-        );
-        const tileContent = <Record<string, any>>doc.content;
-
-        const ecies : Ecies = {
-            iv: Buffer.from(tileContent.masterSeed.iv.data),
-            ephemPublicKey: Buffer.from(tileContent.masterSeed.ephemPublicKey.data),
-            ciphertext: Buffer.from(tileContent.masterSeed.ciphertext.data),
-            mac: Buffer.from(tileContent.masterSeed.mac.data)
-        }
-
-        const seed = Buffer.from(this.threeIdProvider.keychain._keyring.seed);
-        const decryptedMastedSeed = await this.decrypt(seed, ecies);
-        
-        return decryptedMastedSeed.toString();
-    }
-
-    /**
-     * Remove the provided authenticator
-     * @param pubKey the public key derived from prvkey that will be used as authId
-     * @returns bool 
-     */
-    async removeAuthenticator(pubKey: string): Promise<any> {
-        await this.threeIdProvider.keychain.remove(pubKey);
-        await this.threeIdProvider.keychain.commit();
-        return this.threeIdProvider;
-    }
+    return this.threeIdProvider
+  }
 }
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,136 +1,145 @@
-import { expect } from 'chai';
+import { expect } from 'chai'
 import { randomString } from '@stablelib/random'
-import { CeramicSDK } from '../main';
+import { CeramicSDK } from '../main'
 
-import { getPublic } from "@toruslabs/eccrypto";
+import { getPublic } from '@toruslabs/eccrypto'
 
-const pauseSeconds = (sec: number) => new Promise((res) => setTimeout(res, sec * 1000));
+const pauseSeconds = (sec: number) => new Promise(res => setTimeout(res, sec * 1000))
 
-const NODE_URL_3BOXLABS = 'https://ceramic-clay.3boxlabs.com';
-const NODE_URL_TESTNET = 'https://gateway-clay.ceramic.network';
-const NODE_URL_MAINNET = 'https://gateway.ceramic.network';
+const NODE_URL_3BOXLABS = 'https://ceramic-clay.3boxlabs.com'
+const NODE_URL_TESTNET = 'https://gateway-clay.ceramic.network'
+const NODE_URL_MAINNET = 'https://gateway.ceramic.network'
+const randPrivateKey = () => randomString(32, '0123456789abcdef')
 
-describe("Initialize a new Decentralized Identifier.", () => {
-    it("Should initialize a DID based on privatekey", async () => {
+describe('Initialize a new Decentralized Identifier.', () => {
+  it('Should initialize a DID based on privatekey', async () => {
+    const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS)
+    const myPrvkey = randPrivateKey() // Length need to be 32
+    const res = await sdkClient.initialize(myPrvkey, 'pubkeyToUseAsAuthId', 'provider')
+    expect(res).to.match(/did:3:.*/)
+    const meta = (await sdkClient.getMeta()) as any
+    expect(meta.content.masterSeed).to.not.empty
+    expect(Object.keys(meta.content.authenticators).length).to.equal(1)
+    expect(meta.content.authenticators['pubkeyToUseAsAuthId']).to.equal('provider')
+    // expect(res).to.equal('did:3:kjzl6cwe1jw1494fmr39v7jqm87qkukqt38cf6fvtk091qcl795u1n4okkj5b7w')
+  }).timeout(30000)
+})
 
-        const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS);
-        const myPrvkey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // Length need to be 32
-        const res = await sdkClient.initialize(myPrvkey, "pubkeyToUseAsAuthId");
-        expect(res).to.equal("did:3:kjzl6cwe1jw1494fmr39v7jqm87qkukqt38cf6fvtk091qcl795u1n4okkj5b7w");
-    }).timeout(10000);
-});
+describe('Add a new private key as authSecret', () => {
+  const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS)
 
-describe("Add a new private key as authSecret", () => {
-    const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS);
+  it('Should retrieve the same DID for two different authSecrets', async () => {
+    const myPrvkey = randPrivateKey() // Length need to be 32
+    const myNewPrvkey = randPrivateKey() //  Length need to be 32
 
-    it("Should retrieve the same DID for two different authSecrents", async () => {
-        const myPrvkey = "7yaAGVXHhcLFGXL3uQV0Ne85CDFlZjJa"; // Length need to be 32
-        const myNewPrvkey = "RIqUaX127GgnirqYVfMKospZRS3wBhgJ"; //  Length need to be 32
-        
-        const res = await sdkClient.initialize(myPrvkey, "pubkeyToUseAsAuthId");
+    const res = await sdkClient.initialize(myPrvkey, 'pubkeyToUseAsAuthId', 'provider')
 
-        const derivedPubkey2= randomString(20);
-        await sdkClient.addAuthenticator(myNewPrvkey, derivedPubkey2);
+    const derivedPubkey2 = randomString(20)
+    await sdkClient.addAuthenticator(myNewPrvkey, derivedPubkey2, 'provider2')
 
-        const resNewPrvKey = await sdkClient.initialize(myNewPrvkey, derivedPubkey2);
-        expect(resNewPrvKey).to.equal(res);
-    }).timeout(80000);
+    const resNewPrvKey = await sdkClient.initialize(myNewPrvkey, derivedPubkey2, 'provider2')
+    expect(resNewPrvKey).to.equal(res)
 
-    it("Should fail since the key was not added as authSecret", async() => {
-        const myPrvkey = "7yaAGVXHhcLFGXL3uQV0Ne85CDFlZjJa"; // Length need to be 32
-        const myNewPrvkey = "PIqUaX127GgnirqYVfMKospZRS3wBhgJ"; //  Length need to be 32
-        const res = await sdkClient.initialize(myPrvkey, randomString(32));
+    const meta = (await sdkClient.getMeta()) as any
+    expect(meta.content.authenticators[derivedPubkey2]).to.eq('provider2')
+    expect(meta.content.authenticators['pubkeyToUseAsAuthId']).to.eq('provider')
+  }).timeout(80000)
 
-        // myNewPrvkey was not added inside the list of authenticator so this will fail
-        const resNewPrvKey = await sdkClient.initialize(myNewPrvkey, randomString(32));
-        expect(resNewPrvKey).not.to.equal(res);
-    }).timeout(90000);
+  it('Should fail since the key was not added as authSecret', async () => {
+    const myPrvkey = randPrivateKey() // Length need to be 32
+    const myNewPrvkey = randPrivateKey() //  Length need to be 32
+    const res = await sdkClient.initialize(myPrvkey, randomString(32), 'provider')
 
-});
+    // myNewPrvkey was not added inside the list of authenticator so this will fail
+    const resNewPrvKey = await sdkClient.initialize(myNewPrvkey, randomString(32), 'provider')
+    expect(resNewPrvKey).not.to.equal(res)
+  }).timeout(90000)
+})
 
-describe("Remove an authenticator from the the authenticator set", () => {
-    const pubkey1 = "pubkeyToUseAsAuthId";
-    const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS);
+describe('Remove an authenticator from the the authenticator set', () => {
+  const pubkey1 = 'pubkeyToUseAsAuthId'
+  const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS)
 
-    it("Should remove from the authenticator list", async () => {
-        const prvkey = randomString(32);
-        const idw1 = await sdkClient.initialize(prvkey, pubkey1);
-        const chainList = await sdkClient.threeIdProvider.keychain.list();
+  it('Should remove from the authenticator list', async () => {
+    const prvkey = randomString(32)
+    const idw1 = await sdkClient.initialize(prvkey, pubkey1, 'provider')
+    const chainList = await sdkClient.threeIdProvider.keychain.list()
 
-        expect(chainList[0]).to.equal(pubkey1);
+    expect(chainList[0]).to.equal(pubkey1)
 
-        const prvkey2 = randomString(32);
-        const pubkey2 = randomString(32);
-        const authSecret = Buffer.from(prvkey2);
-        await sdkClient.threeIdProvider.keychain.add(pubkey2, authSecret);
-        await pauseSeconds(2);
-        await sdkClient.threeIdProvider.keychain.commit();
+    const prvkey2 = randomString(32)
+    const pubkey2 = randomString(32)
+    const authSecret = Buffer.from(prvkey2)
+    await sdkClient.threeIdProvider.keychain.add(pubkey2, authSecret)
+    await pauseSeconds(2)
+    await sdkClient.threeIdProvider.keychain.commit()
 
-        const newChainList = await sdkClient.threeIdProvider.keychain.list();
+    const newChainList = await sdkClient.threeIdProvider.keychain.list()
 
-        expect(newChainList[0]).to.equal(pubkey1);
-        expect(newChainList[1]).to.equal(pubkey2);
+    expect(newChainList[0]).to.equal(pubkey1)
+    expect(newChainList[1]).to.equal(pubkey2)
 
-        await sdkClient.removeAuthenticator(pubkey2);
-        const listAfterRemoval = await sdkClient.threeIdProvider.keychain.list();
+    await sdkClient.removeAuthenticator(pubkey2)
+    const listAfterRemoval = await sdkClient.threeIdProvider.keychain.list()
 
-        expect(listAfterRemoval.length).to.equal(1);
-        expect(listAfterRemoval[0]).to.equal(pubkey1);
-    }).timeout(90000);
-});
+    expect(listAfterRemoval.length).to.equal(1)
+    expect(listAfterRemoval[0]).to.equal(pubkey1)
 
+    const meta = (await sdkClient.getMeta()) as any
+    expect(meta.content.authenticators[pubkey2]).to.undefined
+  }).timeout(90000)
+})
 
-describe("Create a new tiled document", () => {
-    it("Store the encrypted seed inside masterSeed field", async () => {
-        const pubkey = "pubkeyToUseAsAuthId";
-        const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS);
-        const myPrvkey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // Length need to be 32
-        const res = await sdkClient.initialize(myPrvkey, pubkey);
-        const seed = sdkClient.threeIdProvider.keychain._keyring.seed;
+describe('Create a new tiled document', () => {
+  it('Store the encrypted seed inside masterSeed field', async () => {
+    const pubkey = 'pubkeyToUseAsAuthId'
+    const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS)
+    const myPrvkey = randPrivateKey() // Length need to be 32
+    await sdkClient.initialize(myPrvkey, pubkey, 'provider')
 
-        const doc = await sdkClient.initializeMasterSeed(myPrvkey);
+    // Retreive the encrypted and stored private key and compare it with myprvkey
 
-        // Retreive the encrypted and stored private key and compare it with myprvkey
+    const retrievedMasterSeed = await sdkClient.getMasterSeed()
+    expect(retrievedMasterSeed).to.equal(myPrvkey)
+  }).timeout(80000)
+})
 
-        const retrievedMasterSeed = await sdkClient.getMasterSeed();
-        expect(retrievedMasterSeed).to.equal(myPrvkey);
-    }).timeout(80000);
-});
+describe('Encrypt/decrypt', () => {
+  it('Encrypt', async () => {
+    const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS)
+    const myPrvkey = randPrivateKey() // Length need to be 32
+    const res = await sdkClient.initialize(myPrvkey, 'pubkeyToUseAsAuthId', 'provider')
 
-describe("Encrypt/decrypt", () => {
-    it("Encrypt", async () => {
-        const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS);
-        const myPrvkey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; // Length need to be 32
-        const res = await sdkClient.initialize(myPrvkey, "pubkeyToUseAsAuthId");
+    const seed = Buffer.from(sdkClient.threeIdProvider.keychain._keyring.seed)
+    const publicKeyA = getPublic(seed) // size: 65
+    const encrypt = await sdkClient.encrypt(publicKeyA, myPrvkey)
 
-        const seed = Buffer.from(sdkClient.threeIdProvider.keychain._keyring.seed);
-        var publicKeyA =  getPublic(seed);// size: 65
-        const encrypt = await sdkClient.encrypt(publicKeyA, myPrvkey);
+    const decrypt = await sdkClient.decrypt(seed, encrypt)
+    const str = decrypt.toString()
+    expect(myPrvkey).equal(str)
+    const master = await sdkClient.getMasterSeed()
+    expect(master).to.equal(myPrvkey)
+  }).timeout(80000)
+})
 
-        const decrypt = await sdkClient.decrypt(seed, encrypt);
-        const str = decrypt.toString();
-        expect(myPrvkey).equal(str);
-        const master = await sdkClient.getMasterSeed();
-        expect(master).to.equal(myPrvkey);
-    }).timeout(80000);
-});
+describe('Check masterSeed field not updated', () => {
+  const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS)
 
-describe.only("Check masterSeed field not updated", () => {
-    const sdkClient = new CeramicSDK(NODE_URL_3BOXLABS);
+  it('Should not update the masterSeed field with different authID', async () => {
+    const myPrvkey = randPrivateKey()
+    const res = await sdkClient.initialize(myPrvkey, 'pubkeyToUseAsAuthId1', 'provider')
+    const docBefore = await sdkClient.initializeMasterSeed(myPrvkey, 'pubkey', 'provider')
 
-    it("Should not update the masterSeed field with different authID", async () => {
-        
-        const myPrvkey = "RIqUaX127GgnirqYVfMKospZRS3wBhgJ";
-        const res = await sdkClient.initialize(myPrvkey, "pubkeyToUseAsAuthId1");
-        const docBefore = await sdkClient.initializeMasterSeed(myPrvkey);
+    const res2 = await sdkClient.initialize(myPrvkey, 'pubkeyToUseAsAuthId2', 'provider')
+    const docAfter = await sdkClient.initializeMasterSeed(myPrvkey, 'pubkey', 'provider')
 
-        const res2 = await sdkClient.initialize(myPrvkey, "pubkeyToUseAsAuthId2");
-        const docAfter = await sdkClient.initializeMasterSeed(myPrvkey);
-
-        expect(res).eq(res2);
-        expect(docAfter.controllers[0]).to.equal(docBefore.controllers[0]);
-        expect( (<Record<string, any>>docAfter.content).masterSeed.iv ).deep.equal( (<Record<string, any>>docAfter.content).masterSeed.iv);
-        expect( (<Record<string, any>>docAfter.content).masterSeed.ciphertext ).deep.equal( (<Record<string, any>>docAfter.content).masterSeed.ciphertext);
-    }).timeout(90000);
-
-});
+    expect(res).eq(res2)
+    expect(docAfter.controllers[0]).to.equal(docBefore.controllers[0])
+    expect((<Record<string, any>>docAfter.content).masterSeed.iv).deep.equal(
+      (<Record<string, any>>docAfter.content).masterSeed.iv
+    )
+    expect((<Record<string, any>>docAfter.content).masterSeed.ciphertext).deep.equal(
+      (<Record<string, any>>docAfter.content).masterSeed.ciphertext
+    )
+  }).timeout(90000)
+})
